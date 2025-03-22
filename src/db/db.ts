3a335873eddd3af8c84b7411b6188eb219368e45
Alexandre Trotel <trotelalexandre@proton.me>
2025-03-22 17:45:11 +0100

    fix: update database connection and dependencies

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,7 +1,7 @@
-import { drizzle } from "drizzle-orm/libsql";
-import { createClient } from "@libsql/client";
+import { drizzle } from "drizzle-orm/bun-sql";
+import { SQL } from "bun";
 import * as schema from "./schema.js";
 import "dotenv/config";
 
-const client = createClient({ url: process.env.DATABASE_URL! });
+const client = new SQL(process.env.DATABASE_URL!);
 export const db = drizzle({ client, schema });
